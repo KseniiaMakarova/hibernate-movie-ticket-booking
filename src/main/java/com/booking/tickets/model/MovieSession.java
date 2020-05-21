7bb411d18dcfbf08b43c48ce62d416407c2a755d
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-05-21 12:24:55 +0300

    removed unused import from MovieSession class

--- src/main/java/com/booking/tickets/model/MovieSession.java
+++ src/main/java/com/booking/tickets/model/MovieSession.java
@@ -1,7 +1,6 @@
 package com.booking.tickets.model;
 
 import java.time.LocalDateTime;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
