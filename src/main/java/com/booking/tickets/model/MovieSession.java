4c7edbe717fc07808fb999164e08a862a66710d4
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-05-26 17:52:00 +0300

    fixed improper relational namings

--- src/main/java/com/booking/tickets/model/MovieSession.java
+++ src/main/java/com/booking/tickets/model/MovieSession.java
@@ -1,10 +1,12 @@
 package com.booking.tickets.model;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -17,7 +19,9 @@ public class MovieSession {
     @ManyToOne
     private Movie movie;
     @ManyToOne
+    @JoinColumn(name = "cinema_hall_id")
     private CinemaHall cinemaHall;
+    @Column(name = "session_time")
     private LocalDateTime sessionTime;
 
     public Long getId() {
--- src/main/java/com/booking/tickets/model/Ticket.java
+++ src/main/java/com/booking/tickets/model/Ticket.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -14,6 +15,7 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
+    @JoinColumn(name = "movie_session_id")
     private MovieSession movieSession;
     @ManyToOne
     private User user;
