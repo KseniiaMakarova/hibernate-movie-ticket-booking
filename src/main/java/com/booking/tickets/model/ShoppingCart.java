b712aab810601e5f4e79a8c967616aeb44e7af0e
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-05-27 14:26:38 +0300

    removed @GeneratedValue annotation for id in ShoppingCart class, as its id will be derived from User's id

--- src/main/java/com/booking/tickets/model/ShoppingCart.java
+++ src/main/java/com/booking/tickets/model/ShoppingCart.java
@@ -2,8 +2,6 @@ package com.booking.tickets.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -16,7 +14,6 @@ import javax.persistence.Table;
 @Table(name = "shopping_cart")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
     @JoinTable(
