0a1068ebdf86da14a26128ca86bd1b27f2af94cf
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-06-11 11:33:07 +0300

    added security package to be scanned in WebConfig

--- src/main/java/com/booking/tickets/context/WebConfig.java
+++ src/main/java/com/booking/tickets/context/WebConfig.java
@@ -6,6 +6,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.booking.tickets.entity"})
+@ComponentScan(basePackages = {"com.booking.tickets.entity", "com.booking.tickets.security"})
 public class WebConfig {
 }
