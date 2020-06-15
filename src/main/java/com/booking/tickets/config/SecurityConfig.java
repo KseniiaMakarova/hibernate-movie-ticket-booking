41f781c9829c9f3e737bd4e0c860f4f5464ed26e
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-06-15 11:49:07 +0300

    added @Override annotation before overridden method in SecurityConfig

--- src/main/java/com/booking/tickets/config/SecurityConfig.java
+++ src/main/java/com/booking/tickets/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("USER");
     }
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
